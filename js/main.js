aebd301fb78b8a0c400605a9e281e7a8d0aa7b1a
filippo199600 <pippotartaglia@gmail.com>
2023-07-18 16:09:54 +0200

    Feat .innerHTML

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 let userName = prompt("Name");
 let userSurname = prompt("Surname");
 let userColor = prompt("Favourite Color");
+
+let sumInformations = userName + userSurname + userColor;
+
+document.getElementById("userPreferences").innerHTML = sumInformations;
